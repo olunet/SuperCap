15e4f86b23e5a5134dfa5a0faea7c38f0077529b
Erik <erik.r2ni@gmail.com>
2015-09-18 12:20:28 +0300

    Removed fonts folder and modified navbar and package.json.

--- public/todos/config/todos.client.routes.js
+++ public/todos/config/todos.client.routes.js
@@ -1,17 +1,20 @@
 angular.module('todos').config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-		when('/todos', {
-			templateUrl: 'todos/views/list-todos.client.view.html'
-		}).
-		when('/todos/create', {
-			templateUrl: 'todos/views/create-todo.client.view.html'
-		}).
-		when('/todos/:todoId', {
-			templateUrl: 'todos/views/view-todo.client.view.html'
-		}).
-		when('/todos/:todoId/edit', {
-			templateUrl: 'todos/views/edit-todo.client.view.html'
-		});
-	}
+    function ($routeProvider) {
+        $routeProvider.
+                when('/about', {
+                    templateUrl: 'todos/views/about.client.view.html'
+                }).
+                when('/todos', {
+                    templateUrl: 'todos/views/list-todos.client.view.html'
+                }).
+                when('/todos/create', {
+                    templateUrl: 'todos/views/create-todo.client.view.html'
+                }).
+                when('/todos/:todoId', {
+                    templateUrl: 'todos/views/view-todo.client.view.html'
+                }).
+                when('/todos/:todoId/edit', {
+                    templateUrl: 'todos/views/edit-todo.client.view.html'
+                });
+    }
 ]);
