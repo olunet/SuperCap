757fa49d03ca70ff3f221ddf27928e2565cf31ae
KristjanP94 <kristjan.perli@gmail.com>
2015-09-30 12:36:09 +0300

    #35 - Made graph resize on window resize

--- public/js/libs/chartController.js
+++ public/js/libs/chartController.js
@@ -1,45 +1,54 @@
-createChart = function() {
-    
-        var labels = [];
-        var data = []
-    
-        for(var i = 0; i <= 20; i++) {
-            labels.push(i);
-            data.push(0);
-        }
-    
-    	var data = {
-                labels : labels,
-		datasets : [
-			{
-				fillColor : "rgba(0, 0, 0,0)",
-				strokeColor : "#ff0000",
-				pointColor : "#fff",
-				pointStrokeColor : "#444444",
-                                data: data
-			}
-		]
-	}
 
-        var canvas = document.getElementById('chart');
-        canvas.width = $("#canvasParent").width();
-        
-	var context = canvas.getContext('2d');
-	var chart = new Chart(context).Line(data);
-        this.chart = chart;
+
+createChart = function () {
+    var labels = [];
+    var data = []
+
+    for (var i = 0; i <= 20; i++) {
+        labels.push(i);
+        data.push(0);
+    }
+
+    var data = {
+        labels: labels,
+        datasets: [
+            {
+                fillColor: "rgba(0, 0, 0,0)",
+                strokeColor: "#ff0000",
+                pointColor: "#fff",
+                pointStrokeColor: "#444444",
+                data: data
+            }
+        ]
+    }
+
+
+    var canvas = document.getElementById('chart');
+    canvas.width = $("#canvasParent").width();
+    var context = canvas.getContext('2d');
+    var chart = new Chart(context).Line(data);
+    this.chart = chart;
 }
 
-refreshChart = function(anion, cation, electrolyte) {
-        
-        for(var i = 0; i < chart.datasets[0].points.length; i++) {
-            
-            var alt = i / 10;
-            
-            var val = Math.sin(cation * i) * 10 + Math.cos(anion * alt) * 7 + Math.sin(electrolyte * alt) * 12;
 
-            chart.datasets[0].points[i].value = val;
-        }
-        
-        chart.update();
+refreshChart = function (anion, cation, electrolyte) {
+
+    for (var i = 0; i < chart.datasets[0].points.length; i++) {
+
+        var alt = i / 10;
+
+        var val = Math.sin(cation * i) * 10 + Math.cos(anion * alt) * 7 + Math.sin(electrolyte * alt) * 12;
+
+        chart.datasets[0].points[i].value = val;
+    }
+
+    chart.update();
 
 }
+
+
+
+
+
+
+
