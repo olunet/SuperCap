27bd4e2f859008decd33e6a81f9cf278a3551c0b
Erik <erik.r2ni@gmail.com>
2015-10-29 15:44:28 +0200

    Tests for DataController's anionChanged() and cationChanged() functions.
    (https://github.com/vladislavivanistsev/SuperCap/issues/81)

--- public/tests/DataCtr.test.js
+++ public/tests/DataCtr.test.js
@@ -1,54 +1,56 @@
-// Mocked Service
-angular.module('DataCtrl', []).
-        factory('DataService', function ($q) {
-            var userService = {};
-
-
-            // example stub method that returns a promise, e.g. if original method returned $http.get(...)
-            userService.getAnions = function () {
-                var mockUser = {
-                    id: 8888,
-                    name: "test user"
-                };
-                return $q.when(mockUser);
-            };
-
-            // example stub method that returns a promise, e.g. if original method returned $http.get(...)
-            userService.getCations = function () {
-                var mockUser = {
-                    id: 8888,
-                    name: "test user"
-                };
-                return $q.when(mockUser);
-            };
-
-            // example stub method that returns a promise, e.g. if original method returned $http.get(...)
-            userService.getElectrodes = function () {
-                var mockUser = {
-                    id: 8888,
-                    name: "test user"
-                };
-                return $q.when(mockUser);
-            };
-
-            // other stubbed methods
-
-            return userService;
-        });
-
+/* global expect */
 
 describe('DataCtrl tests', function () {
+    // Mocked DataService
+    angular.module('DataCtrl', []).
+            factory('DataService', function ($q) {
+                var DataService = {};
+                // example stub methods that return a promise, e.g. if original method returned $http.get(...)
+                DataService.getAnions = function () {
+                    return $q.when("");
+                };
+                DataService.getCations = function () {
+                    return $q.when("");
+                };
+                DataService.getElectrodes = function () {
+                    return $q.when("");
+                };
+                return DataService;
+            });
 
     beforeEach(function () {
         angular.mock.module('SuperCap');
+        angular.mock.module('DataCtrl');
 
     });
-    beforeEach(module('DataCtrl'));
-
-
     beforeEach(inject(function ($controller, $rootScope, _DataService_) { // inject mocked service
         this.scope = $rootScope.$new();
-
+        this.scope.anions = [
+            {"_id": {"$oid": "5612c0ade4b09f9838bc3ff9"},
+                "label": "FEP", "r": "0.362", "a0": "0.667", "gamma": "1.000",
+                "xyz": "25\nFEP\nP 0.587337 -3.567276 -0.223589\nC 1.497389 -4.787012 -1.589929\nF 0.514155 -2.342882 -1.351679\nC -1.350876 -3.945899 -0.911114\nF 2.110879 -3.052791 0.196974\nC 0.702586 -5.001636 1.238880\nF -0.077403 -2.583557 0.929882\nF 0.708422 -5.851535 -1.986991\nF 2.635278 -5.334049 -1.014105\nC 2.035098 -4.123571 -2.927003\nF -1.284685 -4.168733 -2.276880\nF -2.065655 -2.764927 -0.754043\nC -2.342127 -5.057822 -0.370820\nF 0.817547 -6.290565 0.745651\nF -0.451483 -4.977883 2.007118\nC 1.874562 -4.863293 2.303710\nF 3.103129 -5.068560 1.768347\nF 1.707748 -5.820281 3.287153\nF 1.858367 -3.653929 2.922455\nF -1.764579 -6.286026 -0.323949\nF -3.433599 -5.156859 -1.208845\nF -2.828811 -4.751586 0.861238\nF 1.044994 -3.654298 -3.725767\nF 2.722723 -5.070774 -3.661249\nF 2.906444 -3.112645 -2.668382"},
+            {
+                "_id": {"$oid": "5612c3bfe4b09f9838bc4062"},
+                "label": "TFSI", "r": "0.324", "a0": "0.667", "gamma": "1.000",
+                "xyz": "15\nTFSI\nS -1.482147 -0.871466 0.362480\nO -1.752196 -1.744667 -0.771918\nO -2.242779 -1.039641 1.592403\nN 0.057017 -0.673277 0.749629\nS 1.283369 -0.820305 -0.281900\nO 2.165497 -1.920421 0.086256\nO 1.001857 -0.584119 -1.691869\nC -2.069727 0.851661 -0.243764\nF -1.463318 1.231959 -1.398763\nF -3.422892 0.815386 -0.470993\nF -1.832956 1.813056 0.696966\nC 2.272301 0.722633 0.258302\nF 3.390255 0.828990 -0.528050\nF 2.684864 0.640410 1.554785\nF 1.549821 1.868364 0.111277"
+            }
+        ];
+        this.scope.cations = ([{"_id": {"$oid": "5612c0ade4b09f9838bc3ff9"},
+                "label": "FEP", "r": "0.362", "a0": "0.667", "gamma": "1.000",
+                "xyz": "25\nFEP\nP 0.587337 -3.567276 -0.223589\nC 1.497389 -4.787012 -1.589929\nF 0.514155 -2.342882 -1.351679\nC -1.350876 -3.945899 -0.911114\nF 2.110879 -3.052791 0.196974\nC 0.702586 -5.001636 1.238880\nF -0.077403 -2.583557 0.929882\nF 0.708422 -5.851535 -1.986991\nF 2.635278 -5.334049 -1.014105\nC 2.035098 -4.123571 -2.927003\nF -1.284685 -4.168733 -2.276880\nF -2.065655 -2.764927 -0.754043\nC -2.342127 -5.057822 -0.370820\nF 0.817547 -6.290565 0.745651\nF -0.451483 -4.977883 2.007118\nC 1.874562 -4.863293 2.303710\nF 3.103129 -5.068560 1.768347\nF 1.707748 -5.820281 3.287153\nF 1.858367 -3.653929 2.922455\nF -1.764579 -6.286026 -0.323949\nF -3.433599 -5.156859 -1.208845\nF -2.828811 -4.751586 0.861238\nF 1.044994 -3.654298 -3.725767\nF 2.722723 -5.070774 -3.661249\nF 2.906444 -3.112645 -2.668382"},
+            {
+                "_id": {"$oid": "5612c3bfe4b09f9838bc4062"},
+                "label": "TFSI", "r": "0.324", "a0": "0.667", "gamma": "1.000",
+                "xyz": "15\nTFSI\nS -1.482147 -0.871466 0.362480\nO -1.752196 -1.744667 -0.771918\nO -2.242779 -1.039641 1.592403\nN 0.057017 -0.673277 0.749629\nS 1.283369 -0.820305 -0.281900\nO 2.165497 -1.920421 0.086256\nO 1.001857 -0.584119 -1.691869\nC -2.069727 0.851661 -0.243764\nF -1.463318 1.231959 -1.398763\nF -3.422892 0.815386 -0.470993\nF -1.832956 1.813056 0.696966\nC 2.272301 0.722633 0.258302\nF 3.390255 0.828990 -0.528050\nF 2.684864 0.640410 1.554785\nF 1.549821 1.868364 0.111277"
+            }]);
+        this.scope.electrodes = ([{"_id": {"$oid": "5612c0ade4b09f9838bc3ff9"},
+                "label": "FEP", "r": "0.362", "a0": "0.667", "gamma": "1.000",
+                "xyz": "25\nFEP\nP 0.587337 -3.567276 -0.223589\nC 1.497389 -4.787012 -1.589929\nF 0.514155 -2.342882 -1.351679\nC -1.350876 -3.945899 -0.911114\nF 2.110879 -3.052791 0.196974\nC 0.702586 -5.001636 1.238880\nF -0.077403 -2.583557 0.929882\nF 0.708422 -5.851535 -1.986991\nF 2.635278 -5.334049 -1.014105\nC 2.035098 -4.123571 -2.927003\nF -1.284685 -4.168733 -2.276880\nF -2.065655 -2.764927 -0.754043\nC -2.342127 -5.057822 -0.370820\nF 0.817547 -6.290565 0.745651\nF -0.451483 -4.977883 2.007118\nC 1.874562 -4.863293 2.303710\nF 3.103129 -5.068560 1.768347\nF 1.707748 -5.820281 3.287153\nF 1.858367 -3.653929 2.922455\nF -1.764579 -6.286026 -0.323949\nF -3.433599 -5.156859 -1.208845\nF -2.828811 -4.751586 0.861238\nF 1.044994 -3.654298 -3.725767\nF 2.722723 -5.070774 -3.661249\nF 2.906444 -3.112645 -2.668382"},
+            {
+                "_id": {"$oid": "5612c3bfe4b09f9838bc4062"},
+                "label": "TFSI", "r": "0.324", "a0": "0.667", "gamma": "1.000",
+                "xyz": "15\nTFSI\nS -1.482147 -0.871466 0.362480\nO -1.752196 -1.744667 -0.771918\nO -2.242779 -1.039641 1.592403\nN 0.057017 -0.673277 0.749629\nS 1.283369 -0.820305 -0.281900\nO 2.165497 -1.920421 0.086256\nO 1.001857 -0.584119 -1.691869\nC -2.069727 0.851661 -0.243764\nF -1.463318 1.231959 -1.398763\nF -3.422892 0.815386 -0.470993\nF -1.832956 1.813056 0.696966\nC 2.272301 0.722633 0.258302\nF 3.390255 0.828990 -0.528050\nF 2.684864 0.640410 1.554785\nF 1.549821 1.868364 0.111277"
+            }]);
         this.ctrl = $controller('DataCtrl', {
             $scope: this.scope,
             DataService: _DataService_
@@ -56,36 +58,29 @@ describe('DataCtrl tests', function () {
 
     }));
 
-    it('Expect $scope to have list of colors.', function () {
-//        this.scope.selectedAnion = {
-//            label: "FEP",
-//            r: 0.362,
-//            a0: 0.667,
-//            gamma: 1.000,
-//            xyz: "25\nFEP\nP 0.587337 -3.567276 -0.223589\nC 1.497389 -4.787012 -1.589929\nF 0.514155 -2.342882 -1.351679\nC -1.350876 -3.945899 -0.911114\nF 2.110879 -3.052791 0.196974\nC 0.702586 -5.001636 1.238880\nF -0.077403 -2.583557 0.929882\nF 0.708422 -5.851535 -1.986991\nF 2.635278 -5.334049 -1.014105\nC 2.035098 -4.123571 -2.927003\nF -1.284685 -4.168733 -2.276880\nF -2.065655 -2.764927 -0.754043\nC -2.342127 -5.057822 -0.370820\nF 0.817547 -6.290565 0.745651\nF -0.451483 -4.977883 2.007118\nC 1.874562 -4.863293 2.303710\nF 3.103129 -5.068560 1.768347\nF 1.707748 -5.820281 3.287153\nF 1.858367 -3.653929 2.922455\nF -1.764579 -6.286026 -0.323949\nF -3.433599 -5.156859 -1.208845\nF -2.828811 -4.751586 0.861238\nF 1.044994 -3.654298 -3.725767\nF 2.722723 -5.070774 -3.661249\nF 2.906444 -3.112645 -2.668382"
-//        };
-//
-//        this.scope.selectedCation = {
-//            label: "BMIm",
-//            r: 0.362,
-//            a0: 0.667,
-//            gamma: 2.000,
-//            xyz: "25\nBMIm\nH -5.394543 -0.342591 -0.463496\nC -4.379393 -0.144278 -0.833283\nC -3.351239 -0.253283 0.304663\nH -4.161619 -0.868893 -1.632493\nH -4.370705 0.862912 -1.276752\nC -1.917403 0.013802 -0.188861\nH -3.398104 -1.258398 0.756118\nH -3.600724 0.463200 1.104812\nC -0.901261 -0.089137 0.955576\nH -1.857740 1.018261 -0.638942\nH -1.649233 -0.710040 -0.975548\nN 0.488975 0.153438 0.492099\nH -0.916916 -1.086723 1.412905\nH -1.109745 0.646270 1.743794\nC 1.433980 -0.785781 0.314888\nC 1.011368 1.379527 0.109983\nN 2.546685 -0.197089 -0.162487\nH 1.320919 -1.841869 0.526724\nC 2.301410 1.161417 -0.297727\nH 0.431016 2.292513 0.161915\nC 3.814702 -0.876189 -0.484487\nH 3.055938 1.848476 -0.661618\nH 4.620249 -0.442318 0.119033\nH 3.718863 -1.942113 -0.255223\nH 4.033674 -0.747224 -1.550779"
-//        };
-//        this.scope.selectedElectrode = {
-//            label: "bismuth",
-//            d: 0.20,
-//            f1: 1.1,
-//            f2: 1.8,
-//            f3: 1.9,
-//            g1: 0.0,
-//            g2: 0.0,
-//            g3: 1000000
-//        }
-//        var inputSet = new InputSet(this.scope.selectedAnion, this.scope.selectedCation, this.scope.selectedElectrode);
-//        this.scope.inputSets.push(inputSet);
-//        addNewInputSetHTML(inputSet);
+    it('Expect $scope to have list of colors and an initial inputsets list.', function () {
         expect(this.scope.colors).toBeDefined();
+        expect(this.scope.inputSets).toBeDefined();
         expect(this.scope.colors).toEqual(["#ff0000", "#00ff00", "#0000ff", "#0000000", "#ffff00", "#ff00ff", "#00ffff"]);
+        expect(this.scope.inputSets).toEqual([{id: 0, anion: undefined, cation: undefined, electrode: undefined, dataset: undefined}]);
+
+    });
+
+    it('Anion is selected via anionChanged().', function () {
+        var dummyElement = document.createElement('div');
+        document.getElementById = jasmine.createSpy('a0AnionValue').andReturn(dummyElement);
+        this.scope.selectedAnion = this.scope.anions[0];
+        this.scope.anionChanged();
+        expect(this.scope.selectedAnion).toBeDefined();
+        expect(this.scope.inputSets[0].anion.label).toEqual("FEP");
+    });
+
+    it('Cation is selected via cationChanged().', function () {
+        var dummyElement = document.createElement('div');
+        document.getElementById = jasmine.createSpy('a0CationValue').andReturn(dummyElement);
+        this.scope.selectedCation = this.scope.cations[1];
+        this.scope.cationChanged();
+        expect(this.scope.selectedCation).toBeDefined();
+        expect(this.scope.inputSets[0].cation.label).toEqual("TFSI");
     });
 });
