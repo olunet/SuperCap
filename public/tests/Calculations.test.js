688f607e9bd8fdf870a95e0d5902cd9f0f284048
Erik <erik.r2ni@gmail.com>
2015-11-08 21:33:52 +0200

    Tests: calculateU2s and calculateCs
    (https://github.com/vladislavivanistsev/SuperCap/issues/81)

--- public/tests/Calculations.test.js
+++ public/tests/Calculations.test.js
@@ -3,10 +3,18 @@
 describe('Calculations tests', function () {
 
     it('calculateSurfaceCharges must return correct value.', function () {
-        expect(calculateSurfaceCharges(0.362, 0.667, 1.000, {"d": 0.12}, 0.20, [-2, -1.96])).toEqual([-2.547711365696452, -2.496759073167246]);
+        expect(calculateSurfaceCharges(0.3, 0.6, 1.000,
+                {"d": 0.1}, 0.2, [-2, -1])).toEqual([-3.0700028470366574,
+            -1.5350199583370898]);
     });
 
-    it('calculateSurfaceCharges must return correct value.', function () {
-        expect(tanh(0)).toEqual(0);
+    it('calculateU2s must return correct value.', function () {
+        expect(calculateU2s([1, 2], {"f1": 1, "f2": 2, "f3": 2, "g1": 0.0,
+            "g2": 0.0, "g3": 1000000}, 2)).toEqual([-0.7663545781054755,
+            -1.6652899597541926]);
+    });
+
+    it('calculateCs must return correct value.', function () {
+        expect(calculateCs([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5])).toEqual([0.5, 0.5, 0.5, 0.5]);
     });
 });
